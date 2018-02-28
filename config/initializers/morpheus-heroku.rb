c029a6e94d20f5dae67d0b21a79f2520eb69232e
Catalin Ionescu <catalin.ionescu282@gmail.com>
2018-02-28 11:39:54 +0200

    add the morpheus initializer

---
+++ config/initializers/morpheus-heroku.rb
@@ -0,0 +1,14 @@
+MorpheusHeroku.configure do |config|
+  config.app_name = "drluca"
+
+  # config.backup_location = "tmp/latest.dump"
+
+  # By default, we use ActiveRecord::Base.connection_config[:database]
+  # config.database_name = "database_development"
+
+  # Show debugging events
+  # config.log_events = false
+
+  # Your development email. Will not scramble emails containing this string
+  # config.dev_email = "dev@blah.com"
+end
