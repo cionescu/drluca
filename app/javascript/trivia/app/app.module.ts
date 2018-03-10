95adee0b4736ce2c4c2180da9408528b4d003adc
Catalin Ionescu <catalin.ionescu282@gmail.com>
2018-03-10 12:51:06 +0200

    add the ng2 cable module

--- app/javascript/trivia/app/app.module.ts
+++ app/javascript/trivia/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2Cable, Broadcaster } from 'ng2-cable/js/index';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
+  providers: [
+    Ng2Cable,
+    Broadcaster
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
